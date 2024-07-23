7ca645b9eeda0ef0c3f990f16cf0512a77e77c00
AkhilllYadav <87937313+AkhilllYadav@users.noreply.github.com>
2024-07-23 18:10:45 +0530

    Update FooterComponent.jsx

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -30,16 +30,16 @@ const FooterComponent = () => {
             <p>Email: <a href="mailto:example@example.com">akhilyadawa@gmail.com</a></p>
             <p>Phone: <a href="tel:+1234567890">+918948890610</a></p>
             <div className="social-media">
-              <a href="https://twitter.com/username" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-twitter"></i>
+              <a href="https://x.com/Akhilll_yad" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-x-twitter"></i>
               </a>
-              <a href="https://linkedin.com/in/username" target="_blank" rel="noopener noreferrer">
+              <a href="https://www.linkedin.com/in/akhilesh-kumar-yadav-61501b1b2/" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-linkedin-in"></i>
               </a>
-              <a href="https://github.com/username" target="_blank" rel="noopener noreferrer">
+              <a href="https://github.com/AkhilllYadav/" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-github"></i>
               </a>
-              <a href="https://instagram.com/username" target="_blank" rel="noopener noreferrer">
+              <a href="https://www.instagram.com/akhilll_yad/" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-instagram"></i>
               </a>
             </div>
