72a5e8fcd6907b89581638cb4130c09053072ffe
AkhilllYadav <87937313+AkhilllYadav@users.noreply.github.com>
2024-08-16 17:28:41 +0530

    Update Certificate.jsx

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -7,6 +7,11 @@ import './styles/Certificate.css'; // Import custom CSS for spacing
 function Certificate() {
   const cardData = [
     {
+      title: 'IBM AI Developer Professional Certificate',
+      image: 'https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~230298V5KV39/CERTIFICATE_LANDING_PAGE~230298V5KV39.jpeg',
+      text: 'Offered by IBM',
+    },
+    {
       title: 'The Complete 2024 Web Development Bootcamp',
       image: 'https://udemy-certificate.s3.amazonaws.com/image/UC-3aa28803-d239-41df-a37d-2225110c8a76.jpg?v=1712639310000',
       text: 'Taught by Dr. Angela Yu, Developer and Lead Instructor on Udemy.',
@@ -15,11 +20,6 @@ function Certificate() {
       title: 'Java 17 Masterclass',
       image: 'https://udemy-certificate.s3.amazonaws.com/image/UC-39dc2830-bc11-46d8-b315-41f23f2ddd54.jpg?v=1711993510000',
       text: 'Taught by Tim Buchalka, Lead Instructor on Udemy.',
-    },
-    {
-      title: 'IBM AI Developer Professional Certificate',
-      image: 'https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~230298V5KV39/CERTIFICATE_LANDING_PAGE~230298V5KV39.jpeg',
-      text: 'Offered by IBM',
     }
    
    
