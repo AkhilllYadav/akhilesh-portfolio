528f397e677b1f0e026dfb37d2d81f41eaecc873
Akhilesh Yadav <87937313+AkhilllYadav@users.noreply.github.com>
2025-02-18 12:36:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import HeroComponent from './components/HeroComponent';
 import Projects from './components/Projects';
+import IncludeAI from './components/IncludeAI';
 import SkillsSection from './components/SkillsSection';
 
 function App() {
@@ -26,7 +27,8 @@ function App() {
           imageUrl="https://media.licdn.com/media/AAYQAQSOAAgAAQAAAAAAAB-zrMZEDXI2T62PSuT6kpB6qg.png"
           cvLink="https://docs.google.com/document/d/1D5i8O6ZLhjNACBYBH7_bxlKTA5TQwADz7Rcy7LaIqw8/edit?usp=sharing" // Replace with your actual CV link
 />
-
+            
+      <IncludeAI/>
       <AboutSection/>
             <EducationComponent education={educationData} />
       <Experience/>
