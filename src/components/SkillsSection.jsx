1bf2b6e04fc4758159ba9261aec2b4ebd619e81b
Akhilesh Yadav <87937313+AkhilllYadav@users.noreply.github.com>
2025-02-18 11:39:11 +0530

    Update SkillsSection.jsx

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -6,32 +6,35 @@ import './styles/SkillsSection.css'; // Custom CSS for Skills Section
 
 const SkillsSection = () => {
 
+  const skillCategories = [
+    'Languages',
+    'Data Analysis & Visualization',
+    'Machine Learning & AI',
+    'Databases',
+    'Tools & Technologies',
+    'Software Engineering',
+    'Frameworks'
+  ];
+
   return (
-    <Container className="skills-section" id='skills' >
+    <Container className="skills-section" id='skills'>
       <h2 className="text-center mb-4">My Skills</h2>
       <Table responsive="md" className="skills-table">
         <thead>
           <tr>
-            <th>Languages</th>
-            <th>Frontend Technologies</th>
-            <th>Tools & Technologies</th>
-            <th>Frameworks</th>
-            
+            {skillCategories.map(category => (
+              <th key={category}>{category}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {Array.from({ length: Math.max(
-            skillsData['Languages'].length,
-            skillsData['Frontend Technologies'].length,
-            skillsData['Tools & Technologies'].length,
-            skillsData['Frameworks'].length,
-          ) }).map((_, rowIndex) => (
+          {Array.from({ length: Math.max(...skillCategories.map(category => skillsData[category]?.length || 0)) }).map((_, rowIndex) => (
             <tr key={rowIndex}>
-              <td>{skillsData['Languages'][rowIndex] && <SkillComponent {...skillsData['Languages'][rowIndex]} />}</td>
-              <td>{skillsData['Frontend Technologies'][rowIndex] && <SkillComponent {...skillsData['Frontend Technologies'][rowIndex]} />}</td>
-              <td>{skillsData['Tools & Technologies'][rowIndex] && <SkillComponent {...skillsData['Tools & Technologies'][rowIndex]} />}</td>
-              <td>{skillsData['Frameworks'][rowIndex] && <SkillComponent {...skillsData['Frameworks'][rowIndex]} />}</td>
-              
+              {skillCategories.map(category => (
+                <td key={category}>
+                  {skillsData[category] && skillsData[category][rowIndex] && <SkillComponent {...skillsData[category][rowIndex]} />}
+                </td>
+              ))}
             </tr>
           ))}
         </tbody>
