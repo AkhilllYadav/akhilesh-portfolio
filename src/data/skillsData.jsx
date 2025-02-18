ee797537f3522deb2baa2d4e741de8e1238d60a2
Akhilesh Yadav <87937313+AkhilllYadav@users.noreply.github.com>
2025-02-18 11:35:31 +0530

    Update skillsData.jsx

--- src/data/skillsData.jsx
+++ src/data/skillsData.jsx
@@ -10,28 +10,41 @@ const skillsData = {
     { title: 'Pandas', image: 'https://pandas.pydata.org/static/img/pandas_white.svg' },
     { title: 'Matplotlib', image: 'https://matplotlib.org/_static/images/logo2.svg' },
     { title: 'Seaborn', image: 'https://seaborn.pydata.org/_static/logo-wide-lightbg.svg' },
+    { title: 'Tableau', image: 'https://upload.wikimedia.org/wikipedia/commons/4/45/Tableau_Logo.png' },
+    { title: 'Exploratory Data Analysis (EDA)', image: 'https://miro.medium.com/max/875/1*iNdkYNNJdfXPkV2YMLq8kg.png' },
+    { title: 'Data Wrangling', image: 'https://dataschool.com/assets/img/datacleaning.jpg' },
+    { title: 'Data Visualization', image: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/Data_Visualization.svg' },
   ],
-  'Machine Learning Frameworks': [
+  'Machine Learning & AI': [
     { title: 'TensorFlow', image: 'https://www.tensorflow.org/images/tf_logo_social.png' },
     { title: 'Scikit-Learn', image: 'https://scikit-learn.org/stable/_static/scikit-learn-logo-small.png' },
     { title: 'Keras', image: 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Keras_logo.svg' },
+    { title: 'Deep Learning', image: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Deep_Learning.png' },
+    { title: 'Natural Language Processing (NLP)', image: 'https://upload.wikimedia.org/wikipedia/commons/8/8b/NLP_Icon.png' },
+    { title: 'Artificial Intelligence (AI)', image: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/AI_Logo.png' },
+    { title: 'Machine Learning', image: 'https://upload.wikimedia.org/wikipedia/commons/4/4a/Machine_Learning.png' },
+    { title: 'Pattern Recognition', image: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Pattern_Recognition.png' },
   ],
   'Databases': [
     { title: 'MongoDB', image: 'https://www.mongodb.com/assets/images/global/leaf.svg' },
     { title: 'SQL', image: 'https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png' },
-  ],
-  'Frontend Technologies': [
-    { title: 'HTML', image: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg' },
-    { title: 'CSS', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg' },
-    { title: 'Bootstrap', image: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg' },
+    { title: 'Database Management System (DBMS)', image: 'https://upload.wikimedia.org/wikipedia/commons/e/ed/Database.svg' },
   ],
   'Tools & Technologies': [
     { title: 'Git', image: 'https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png' },
     { title: 'Firebase', image: 'https://firebase.google.com/downloads/brand-guidelines/PNG/logo-vertical.png' },
+    { title: 'Jupyter Notebook', image: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Jupyter_logo.svg' },
+    { title: 'Google Gemini', image: 'https://upload.wikimedia.org/wikipedia/commons/e/e7/Gemini_AI.png' },
+  ],
+  'Software Engineering': [
+    { title: 'Object-Oriented Programming (OOP)', image: 'https://upload.wikimedia.org/wikipedia/commons/9/93/OO_Programming.svg' },
+    { title: 'Data Structures', image: 'https://upload.wikimedia.org/wikipedia/commons/b/bf/Data_Structure.png' },
+    { title: 'Analytical Skills', image: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Analytics.png' },
+    { title: 'Business Analytics', image: 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Business_Analytics.png' },
+    { title: 'Statistics', image: 'https://upload.wikimedia.org/wikipedia/commons/e/ed/Statistics_Icon.svg' },
   ],
   'Frameworks': [
     { title: 'React JS', image: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg' },
-    // { title: 'Spring Boot', image: 'https://spring.io/images/icon-spring-boot-512x512.png' },
   ],
 };
 
